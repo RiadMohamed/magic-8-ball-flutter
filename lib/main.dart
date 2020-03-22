4ff8559b16610ab73ae9ec4cf79f043d275f4a70
Riad Mohamed <riaduk1@gmail.com>
2020-03-22 18:55:20 +0200

    Step 3 complete.

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,27 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[600],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
         title: Text("Ask Me Anything"),
       ),
-      body: Container(
-        color: Colors.blue[600],
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image.asset('images/ball1.png'),
       ),
     );
   }
